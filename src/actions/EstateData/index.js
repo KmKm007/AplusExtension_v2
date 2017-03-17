cbeb9d0955c7a6f4aa9a0b3d8fdb2ae728bdbeeb
hxm <dgdkhxm@163.com>
2017-03-17 12:03:46 +0800

    action creators

---
+++ src/actions/EstateData/index.js
@@ -0,0 +1,27 @@
+import * as types from '../../actionTypes/EstateData'
+
+export const changeOrderRule = orderRule => ({
+  type: types.CHANGE_ORDER_RULE,
+  orderRule
+})
+
+export const fetchLastPage = () => ({
+  type: types.FETCH_LAST_PAGE
+})
+
+export const fetchFirstPage = () => ({
+  type: types.FETCH_FIRST_PAGE
+})
+
+export const fetchSelectedPage = selectedPage => ({
+  type: types.FETCH_SELECTED_PAGE,
+  selectedPage
+})
+
+export const fetchNextPage = () => ({
+  type: types.FETCH_NEXT_PAGE
+})
+
+export const fetchLastPage = () => ({
+  type: types.FETCH_LAST_PAGE
+})
