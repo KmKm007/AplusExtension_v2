5563a031ce2a2e47a709dac9c0d800a7773dc9b7
hxm <dgdkhxm@163.com>
2017-03-20 17:35:26 +0800

    新增完成加载regionList后的reducer

--- src/reducers/estateData/dataStatusReducer.js
+++ src/reducers/estateData/dataStatusReducer.js
@@ -1,20 +1,49 @@
 import types from '../../actionTypes/EstateData'
 
 const initialState = {
-  isDataFetched: false
+  isDataFetched: false,
+  isShowSearchBar: false,
+  isRegionListFetched: false
 }
 
 const finishDataFetch = state => {
-    return {
-      ...state,
-      isDataFetched: true
-    }
+  return {
+    ...state,
+    isDataFetched: true
+  }
+}
+
+const finishRegionListFetch = state => {
+  return {
+    ...state,
+    isRegionListFetched: true
+  }
+}
+
+const showSearchBar = state => {
+  return {
+    ...state,
+    isShowSearchBar: true
+  }
+}
+
+const hideSearchBar = state => {
+  return {
+    ...state,
+    isShowSearchBar: false
+  }
 }
 
 const dataStatus = (state = initialState, action) => {
   switch (action.type) {
     case types.FINISH_DATA_FETCH:
       return finishDataFetch(state)
+    case types.SHOW_SEARCH_BAR:
+      return showSearchBar(state)
+    case types.HIDE_SEARCH_BAR:
+      return hideSearchBar(state)
+    case types.FINISH_REGION_LIST_FETCH:
+      return finishRegionListFetch(state)
     default:
       return state
   }
