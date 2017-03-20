27d594ab13c96f0b35d49107e4d99503f7abb64f
hxm <dgdkhxm@163.com>
2017-03-20 09:10:14 +0800

    删除重复文件

--- src/appLink/urls.js
+++
@@ -1,4 +0,0 @@
-const url = 'http://10.17.1.58:8888/APlusExtension/data/estate/default'
-const exportUrl = 'http://10.17.1.58:8888/APlusExtension/data/estate/defaultExport'
-
-export { url, exportUrl }
